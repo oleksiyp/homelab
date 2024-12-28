11ef1c91a80c3b8ae62f775706f289e799a8e6c6
Oleksiy Pylypenko <oleksiy.pylypenko@gmail.com>
2024-12-28 06:11:47 +0100

    Gitea actions

--- dev/gitea.sh
+++ dev/gitea.sh
@@ -1,2 +1,16 @@
+kubectl apply --filename - <<DOC
+kind: Secret
+apiVersion: v1
+metadata:
+  name: gitea-actions
+stringData:
+  secret: RZ0nL9cTl8pILLYVJUtewZ8GVN5Q00pBm7ePYFep
+DOC
 helm repo add gitea-charts https://dl.gitea.com/charts/
-helm upgrade --install gitea gitea-charts/gitea --set actions.enabled=true
+helm repo update gitea-charts
+helm upgrade --values - --install gitea gitea-charts/gitea <<DOC
+actions:
+  enabled: true
+  existingSecret: gitea-actions
+  existingSecretKey: secret
+DOC
